ccb665425b09172a099a8500ed4a0e37396add35
caima0 <andriy.mos.my@gmail.com>
2026-01-11 19:36:46 +0100

    Main Page and layout base visuals

--- src/layout/root-layout.tsx
+++ src/layout/root-layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 export default function RootLayout() {
   return (
-    <div>
+    <div className="max-w-5xl mx-auto px-2 py-4">
       <Outlet />
     </div>
   );
