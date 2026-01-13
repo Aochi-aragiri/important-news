8427abe076ed4b075c89f49361eeb9a6aae905c7
Aochi-aragiri <Kfurlet21@gmail.com>
2026-01-13 20:15:37 +0200

    post-page-commit

--- src/layout/root-layout.tsx
+++ src/layout/root-layout.tsx
@@ -3,6 +3,9 @@ import { Outlet } from 'react-router';
 export default function RootLayout() {
   return (
     <div className="max-w-5xl mx-auto px-2 py-4">
+      <p className="flex justify-center mt-15 mb-10 text-3xl bg-primary rounded-2xl p-6 text-white font-bold">
+        Important news!
+      </p>
       <Outlet />
     </div>
   );
