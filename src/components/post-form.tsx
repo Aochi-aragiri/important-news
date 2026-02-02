68a779061228c0300a5b883ed881d1e54d888098
Sed225 <pavlolehiv@gmail.com>
2026-02-02 21:02:07 +0200

    Add post preview feature to PostForm component

--- src/components/post-form.tsx
+++ src/components/post-form.tsx
@@ -6,6 +6,8 @@ import { ChevronLeft } from 'lucide-react';
 import { useFormContext } from 'react-hook-form';
 import { Link } from 'react-router';
 import type { PostFormData } from './post-form.config';
+import { useState } from 'react';
+import PostData from './post-data';
 
 interface PostFormProps {
   backHref: string;
@@ -13,80 +15,99 @@ interface PostFormProps {
 }
 
 export default function PostForm({ backHref, pending }: PostFormProps) {
+  const [isPreview, setIsPreview] = useState(false);
+
   const form = useFormContext<PostFormData>();
 
+  const title = form.watch('title');
+  const body = form.watch('body');
   const imageUrl = form.watch('imageUrl');
+  const tags = form.watch('tags');
 
   return (
     <div className="max-w-5xl mx-auto px-2 py-4">
-      <div className="flex justify-between mb-2">
-        <div className="w-full mr-4">
-          <Button
-            asChild
-            variant={'outline'}
-            className="mb-4 rounded-2 bg-[#D9D9D9]"
-          >
-            <Link to={backHref}>
-              <ChevronLeft /> Back
-            </Link>
-          </Button>
-          <FieldGroup className="gap-2">
-            <Field className="gap-0">
-              <FieldLabel className="text-2xl font-bold">
-                Edit Title:
-              </FieldLabel>
-              <Input
-                disabled={pending}
-                placeholder="Your Important New"
-                className="bg-[#D9D9D9] rounded-2"
-                aria-invalid={!!form.formState.errors.title}
-                {...form.register('title')}
-              />
-            </Field>
-            <Field className="gap-0">
-              <FieldLabel className="text-2xl font-bold">Edit Text:</FieldLabel>
-              <Textarea
-                disabled={pending}
-                placeholder="Description of your Important new"
-                className="h-100 bg-[#D9D9D9] rounded-2"
-                aria-invalid={!!form.formState.errors.body}
-                {...form.register('body')}
-              />
-            </Field>
-          </FieldGroup>
-        </div>
-        <Field className="w-full">
-          <FieldLabel>Image</FieldLabel>
-          <Input
-            disabled={pending}
-            type="url"
-            placeholder="https://images.com/image.jpg"
-            aria-invalid={!!form.formState.errors.imageUrl}
-            {...form.register('imageUrl')}
-          />
-          {imageUrl ? (
-            <img
-              className="mb-2 w-auto h-110 object-cover rounded-lg"
-              src={imageUrl}
-              alt="new`s image"
+      {isPreview ? (
+        <PostData
+          title={title}
+          createdAt={new Date().toISOString()}
+          body={body}
+          imageUrl={imageUrl}
+          tags={tags}
+        />
+      ) : (
+        <div className="flex justify-between mb-2">
+          <div className="w-full mr-4">
+            <Button
+              asChild
+              variant={'outline'}
+              className="mb-4 rounded-2 bg-[#D9D9D9]"
+            >
+              <Link to={backHref}>
+                <ChevronLeft /> Back
+              </Link>
+            </Button>
+            <FieldGroup className="gap-2">
+              <Field className="gap-0">
+                <FieldLabel className="text-2xl font-bold">
+                  Edit Title:
+                </FieldLabel>
+                <Input
+                  disabled={pending}
+                  placeholder="Your Important New"
+                  className="bg-[#D9D9D9] rounded-2"
+                  aria-invalid={!!form.formState.errors.title}
+                  {...form.register('title')}
+                />
+              </Field>
+              <Field className="gap-0">
+                <FieldLabel className="text-2xl font-bold">
+                  Edit Text:
+                </FieldLabel>
+                <Textarea
+                  disabled={pending}
+                  placeholder="Description of your Important new"
+                  className="h-100 bg-[#D9D9D9] rounded-2"
+                  aria-invalid={!!form.formState.errors.body}
+                  {...form.register('body')}
+                />
+              </Field>
+            </FieldGroup>
+          </div>
+          <Field className="w-full">
+            <FieldLabel>Image</FieldLabel>
+            <Input
+              disabled={pending}
+              type="url"
+              placeholder="https://images.com/image.jpg"
+              aria-invalid={!!form.formState.errors.imageUrl}
+              {...form.register('imageUrl')}
             />
-          ) : (
-            <div className="mb-2 h-110 bg-[#D9D9D9] rounded-lg" />
-          )}
-          <FieldGroup>
-            <Field className="gap-0">
-              <FieldLabel className="text-2xl font-bold">Edit Tags:</FieldLabel>
-              <Input
-                disabled={pending}
-                placeholder="Tags of your Important New"
-                className="bg-[#D9D9D9] rounded-2"
-                aria-invalid={!!form.formState.errors.tags}
-                {...form.register('tags')}
+            {imageUrl ? (
+              <img
+                className="mb-2 w-auto h-110 object-cover rounded-lg"
+                src={imageUrl}
+                alt="new`s image"
               />
-            </Field>
-          </FieldGroup>
-        </Field>
-      </div>
+            ) : (
+              <div className="mb-2 h-110 bg-[#D9D9D9] rounded-lg" />
+            )}
+            <FieldGroup>
+              <Field className="gap-0">
+                <FieldLabel className="text-2xl font-bold">
+                  Edit Tags:
+                </FieldLabel>
+                <Input
+                  disabled={pending}
+                  placeholder="Tags of your Important New"
+                  className="bg-[#D9D9D9] rounded-2"
+                  aria-invalid={!!form.formState.errors.tags}
+                  {...form.register('tags')}
+                />
+              </Field>
+            </FieldGroup>
+          </Field>
+        </div>
+      )}
       <div className="flex justify-between ">
         <Button
           type="submit"
@@ -95,10 +116,13 @@ export default function PostForm({ backHref, pending }: PostFormProps) {
           Confirm
         </Button>
         <Button
+          onClick={() => {
+            setIsPreview(!isPreview);
+          }}
           type="button"
           className="text-lg font-bold py-6 px-10 bg-[#AA9A4E] text-white rounded-xl"
         >
-          Preview
+          {isPreview ? 'Edit' : 'Preview'}
         </Button>
       </div>
     </div>
